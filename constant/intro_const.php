7d7bec7732412a1bb01a5b5d5efc8382260989ab
ikkrit <johan.car@protonmail.com>
2022-12-22 15:54:26 +0100

    algo

---
+++ constant/intro_const.php
@@ -0,0 +1,12 @@
+<?php
+
+    define("INTRO_1", "Bienvenue dans le jeux test\n");
+    define("INTRO_2", "Qui n'a aucun sens...\n");
+    define("INTRO_3", "Prepare toi bien...\n");
+
+    define("INTRO_NAME", "Entre ton nom : ");
+    define("INTRO_PSEUDO", "Entre ton pseudo : ");
+    define("INTRO_AGE", "Entre ton age : ");
+    define("INTRO_GENDER", "Entre ton genre : (homme) (femme) (inconnu) : ");
+
+?>
--- index.php
+++ index.php
@@ -1,25 +1,61 @@
 <?php
 
-    $intro_1 = "Bienvenue dans le jeux";
-    $intro_2 = "Qui n'a aucun sens...";
+    include "constant/intro_const.php";
 
-    $intro_name = "Entre t'on nom";
+    // FUNCTION DU JEUX //
 
-    $name;
-    $pseudo;
-    $age;
-    $gender;
+    function age($intro_age)
+    {
+        $age = readline($intro_age);
+        if($age >= 18) {
+            return $age;
+        } else {
+            return false;
+        }
+    }
 
+    function gender($intro_gender)
+    {
+        $gender = strtolower(readline($intro_gender));
+        if($gender == "homme" || $gender == "femme" || $gender == "inconnu") {
+            return $gender;
+        } else {
+            return false;
+        }
+    }
 
+    function present_user(string $name, string $pseudo, int $age, string $gender)
+    {
+        $gender == "homme" ? $p = "un" : $p = "une";
+        echo "Tu t'appel {$name}, ton pseudo est : {$pseudo}, tu as {$age} ans et tu est {$p} {$gender}";
+    } 
 
-    echo $intro_1;
-    echo "<br>";
-    echo $intro_2;
+    function verif_user($name, $pseudo, $age, $gender) {
+        if(!$age) {
+            echo "Ce jeux est pour les 18 ans et plus\n";
+            exit("Game over");
+        }if(!$gender) {
+            echo "Pas compris ton choix...\n";
+            exit("Game over");
+        } else {
+            present_user($name, $pseudo, $age, $gender);
+        }
+    }
 
-    $name = readline($intro_name);
+    
+    // DEBUT DU JEUX // 
 
-    echo $name;
+    echo INTRO_1;
+    echo INTRO_2;
+    echo INTRO_3;
 
+    $name = readline(INTRO_NAME);
+    $pseudo = readline(INTRO_PSEUDO);
+    $age = age(INTRO_AGE);
+    $gender = gender(INTRO_GENDER);
+
+    
+    verif_user($name, $pseudo, $age, $gender);
     
 
 ?>
