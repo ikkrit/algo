cf179267668c82aed80675ea9d2e6700bd051b64
ikkrit <johan.car@protonmail.com>
2022-12-22 12:51:20 +0100

    game

---
+++ form.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/style.css">
+    <title>Game</title>
+</head>
+<body>
+
+    <section class="form-section">
+
+        <div class="signin">
+            <div class="content">
+                <h2>Sign In</h2>
+                <div class="form">
+
+                    <div class="inputBx">
+                        <input type="text" required>
+                        <i>Username</i>
+                    </div>
+
+                    <div class="inputBx">
+                        <input type="password" required>
+                        <i>Password</i>
+                    </div>
+
+                    <div class="links">
+                        <a href="#">Forgot Password</a>
+                        <a href="#">Signup</a>
+                    </div>
+
+                    <div class="inputBx">
+                        <input type="submit" value="login">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,85 +1,25 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/style.css">
-    <title>Game</title>
-</head>
-<body>
+<?php
 
-    <section class="form-section">
+    $intro_1 = "Bienvenue dans le jeux";
+    $intro_2 = "Qui n'a aucun sens...";
 
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+    $intro_name = "Entre t'on nom";
 
-        <div class="signin">
-            <div class="content">
-                <h2>Sign In</h2>
-                <div class="form">
+    $name;
+    $pseudo;
+    $age;
+    $gender;
 
-                    <div class="inputBx">
-                        <input type="text" required>
-                        <i>Username</i>
-                    </div>
 
-                    <div class="inputBx">
-                        <input type="password" required>
-                        <i>Password</i>
-                    </div>
 
-                    <div class="links">
-                        <a href="#">Forgot Password</a>
-                        <a href="#">Signup</a>
-                    </div>
+    echo $intro_1;
+    echo "<br>";
+    echo $intro_2;
 
-                    <div class="inputBx">
-                        <input type="submit" value="login">
-                    </div>
+    $name = readline($intro_name);
 
-                </div>
-            </div>
-        </div>
-    </section>
+    echo $name;
+
     
-</body>
-</html>
+
+?>
